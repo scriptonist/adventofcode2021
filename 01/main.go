854eb3dd3dbb53d1e3e69190ae7d55c023d7c252
scriptonist <scriptonist@gmail.com>
2021-12-01 14:33:37 +0530

    day1

--- 01/main.go
+++ 01/main.go
@@ -1,1 +1,64 @@
+package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func findIncreasedReadings(file string) int {
+	numbers := []int{}
+	bs, err := ioutil.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	for _, line := range strings.Split(string(bs), "\n") {
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+		numbers = append(numbers, n)
+	}
+
+	c := 0
+	for i := 1; i < len(numbers); i++ {
+		if numbers[i] > numbers[i-1] {
+			c++
+		}
+	}
+
+	return c
+}
+
+func findIncreasedReadingsFromSlidingWindow(file string) int {
+	numbers := []int{}
+	bs, err := ioutil.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	for _, line := range strings.Split(string(bs), "\n") {
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+		numbers = append(numbers, n)
+	}
+
+	c := -1
+	prevSum := -1
+	for i := 0; i+2 < len(numbers); i++ {
+		s := numbers[i] + numbers[i+1] + numbers[i+2]
+		if s > prevSum {
+			c++
+		}
+		prevSum = s
+	}
+
+	return c
+}
+
+func main() {
+	fmt.Println(findIncreasedReadings("input.txt"))
+	fmt.Println(findIncreasedReadingsFromSlidingWindow("input.txt"))
+}
