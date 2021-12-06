46b12c43b37a2f43c8ba31d5a6d9393fbf4ab3e8
scriptonist <scriptonist@gmail.com>
2021-12-06 10:30:22 +0530

    day5 part2

--- go/05/main.go
+++ go/05/main.go
@@ -67,7 +67,7 @@ x2,y2: 2
 func part1(segments []lineSegment) int {
 	pointsMap := map[point]int{}
 	for _, segment := range segments {
-		points := findIntermediatePoints(segment.p1, segment.p2)
+		points := findPointsOnVerticalAndHorizontals(segment.p1, segment.p2)
 		for _, p := range points {
 			pointsMap[p]++
 		}
@@ -82,7 +82,29 @@ func part1(segments []lineSegment) int {
 	return result
 }
 
-func findIntermediatePoints(p1, p2 point) []point {
+func part2(segments []lineSegment) int {
+	pointsMap := map[point]int{}
+	for _, segment := range segments {
+		points := findPointsOnVerticalAndHorizontals(segment.p1, segment.p2)
+		points = append(points, findPointsOnDiagonals(segment.p1, segment.p2)...)
+		// fmt.Println(segment, points)
+		for _, p := range points {
+			pointsMap[p]++
+		}
+	}
+	result := 0
+	for _, count := range pointsMap {
+		if count > 1 {
+			result++
+		}
+	}
+	// for k, v := range pointsMap {
+	// 	fmt.Println(k, v)
+	// }
+	return result
+}
+
+func findPointsOnVerticalAndHorizontals(p1, p2 point) []point {
 	var from, to int
 	if p1.x == p2.x {
 		from = p1.y
@@ -105,7 +127,53 @@ func findIntermediatePoints(p1, p2 point) []point {
 	return points
 }
 
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func findPointsOnDiagonals(p1, p2 point) []point {
+	if !(abs(p1.x-p1.y) == abs(p2.x-p2.y)) && !(p1.x+p1.y == p2.x+p2.y) && !(p1.x == p1.y && p2.x == p2.y) {
+		return []point{}
+	}
+	points := []point{}
+
+	if (p1.x == p1.y) && (p2.x == p2.y) {
+		from, to := p1.x, p2.x
+		if from > to {
+			from, to = to, from
+		}
+		for i := from; i <= to; i++ {
+			points = append(points, point{i, i})
+		}
+	} else if p1.x+p1.y == p2.x+p2.y {
+		if p1.x > p2.x {
+			for i, j := p1.x, p1.y; i >= p2.x; i, j = i-1, j+1 {
+				points = append(points, point{i, j})
+			}
+		} else if p1.x < p2.x {
+			for i, j := p1.x, p1.y; i <= p2.x; i, j = i+1, j-1 {
+				points = append(points, point{i, j})
+			}
+		}
+	} else if abs(p1.x-p1.y) == abs(p2.x-p2.y) {
+		if p1.x < p2.x {
+			for i, j := p1.x, p1.y; i <= p2.x; i, j = i+1, j+1 {
+				points = append(points, point{i, j})
+			}
+		} else if p1.x > p2.x {
+			for i, j := p2.x, p2.y; i <= p1.x; i, j = i+1, j+1 {
+				points = append(points, point{i, j})
+			}
+		}
+	}
+	return points
+}
+
 func main() {
 	segments := parseInput("input.txt")
 	fmt.Println(part1(segments))
+	fmt.Println(part2(segments))
 }
--- go/05/main_test.go
+++ go/05/main_test.go
@@ -71,8 +71,119 @@ func Test_findIntermediatePoints(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := findIntermediatePoints(tt.args.p1, tt.args.p2); !reflect.DeepEqual(got, tt.want) {
+			if got := findPointsOnVerticalAndHorizontals(tt.args.p1, tt.args.p2); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("findIntermediatePoints() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findDiagonalPoints(t *testing.T) {
+	type args struct {
+		p1 point
+		p2 point
+	}
+	tests := []struct {
+		name string
+		args args
+		want []point
+	}{
+		{
+			"t1",
+			args{p1: point{9, 7}, p2: point{7, 9}},
+			[]point{
+				{9, 7},
+				{8, 8},
+				{7, 9},
+			},
+		},
+		{
+			"t2",
+			args{p1: point{1, 1}, p2: point{3, 3}},
+			[]point{
+				{1, 1},
+				{2, 2},
+				{3, 3},
+			},
+		},
+		{
+			"t3",
+			args{p1: point{3, 3}, p2: point{5, 1}},
+			[]point{
+				{3, 3},
+				{4, 2},
+				{5, 1},
+			},
+		},
+		{
+			"t4",
+			args{p1: point{3, 3}, p2: point{1, 5}},
+			[]point{
+				{3, 3},
+				{2, 4},
+				{1, 5},
+			},
+		},
+		{
+			"t5",
+			args{p1: point{5, 1}, p2: point{3, 3}},
+			[]point{
+				{5, 1},
+				{4, 2},
+				{3, 3},
+			},
+		},
+		{
+			"t6",
+			args{p1: point{5, 1}, p2: point{3, 3}},
+			[]point{
+				{5, 1},
+				{4, 2},
+				{3, 3},
+			},
+		},
+		{
+			"t7",
+			args{p1: point{0, 0}, p2: point{8, 8}},
+			[]point{
+				{0, 0},
+				{1, 1},
+				{2, 2},
+				{3, 3},
+				{4, 4},
+				{5, 5},
+				{6, 6},
+				{7, 7},
+				{8, 8},
+			},
+		},
+		{
+			"t8",
+			args{p1: point{6, 4}, p2: point{2, 0}},
+			[]point{
+				{2, 0},
+				{3, 1},
+				{4, 2},
+				{5, 3},
+				{6, 4},
+			},
+		},
+		{
+			"t9",
+			args{p1: point{2, 0}, p2: point{6, 4}},
+			[]point{
+				{2, 0},
+				{3, 1},
+				{4, 2},
+				{5, 3},
+				{6, 4},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findPointsOnDiagonals(tt.args.p1, tt.args.p2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findDiagonalPoints() = %v, want %v", got, tt.want)
 			}
 		})
 	}
